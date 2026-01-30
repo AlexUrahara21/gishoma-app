e8038f0dfea0465de1eba7aff7ecfbb5b30a95ea
Komezusenge-Bolice <komezusengebolice@gmail.com>
2026-01-30 17:17:17 +0200

    frontend development

---
+++ gishoma-app-frontend/src/Message.tsx
@@ -0,0 +1,10 @@
+function Message() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <button>I am a React developer passionate to design good and productive systems</button>
+    </div>
+  );
+}
+
+export default Message;
