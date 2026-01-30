4a6c3b47f9823c0c20503eeda6520250ada91a08
Komezusenge-Bolice <komezusengebolice@gmail.com>
2026-01-30 17:37:56 +0200

    addition of css file

--- gishoma-app-frontend/src/Message.tsx
+++ gishoma-app-frontend/src/Message.tsx
@@ -1,8 +1,10 @@
+import "./Message.css";
 function Message() {
+    
   return (
     <div>
-      <h1>Hello React</h1>
-      <button>I am a React developer passionate to design good and productive systems</button>
+      <h1 className="title" >Hello React</h1>
+      <button className="button">I am a React developer passionate to design good and productive systems</button>
     </div>
   );
 }
